c807e0aa95f47a6937da6a12e63d53b763be389a
Aleksandr Vidyakin <tamplers1725@gmail.com>
2022-03-25 13:32:29 +0400

    model

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-type: text/html; charset=UTF-8');
-require_once 'apiClass.php';
-$API = new APIClass($_GET['class'],$_GET['method'], $_POST['params'], $_COOKIE['token']);
+use apiClass;
+$API = new apiClass($_GET['class'],$_GET['method'], $_POST['params'], $_COOKIE['token']);
 $API->callApiFunction();
 echo json_encode($API->resultFunctionCall);
